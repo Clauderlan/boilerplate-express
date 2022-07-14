4e7befc29bfca3c994057ad9b3a5182e0c13578a
Clauderlan <89857887+Clauderlan@users.noreply.github.com>
2022-07-14 16:17:03 -0300

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
+console.log("hello world")
 
-
-
+// Answering the GET Request with HTML file.
+app.get("/root", (req, res) => {
+    absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath);
+})
 
 
 
