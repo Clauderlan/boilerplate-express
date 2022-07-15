360e1c443af0d0e43f0b3ee7477a9e7fafd21d5c
Clauderlan <89857887+Clauderlan@users.noreply.github.com>
2022-07-14 23:23:37 -0300

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -2,9 +2,14 @@ let express = require('express');
 let app = express();
 console.log("hello world")
 
+// Adicionando os arquivos estaticos que o HTML precisa. (CSS) Adding the static files that HTML needs.
+
+let absolutePath = __dirname + "/public";
+app.use("/public", express.static(absolutePath))
+
 // Answering the GET Request with HTML file.
 app.get("/root", (req, res) => {
-    absolutePath = __dirname + "/views/index.html"
+    let absolutePath = __dirname + "/views/index.html"
     res.sendFile(absolutePath);
 })
 
